fd383e50740b2457f6d4bd6bd91ee8474d0d0e93
ilomedia <contact@ilomedia.net>
2019-06-26 11:30:46 +0200

    use snips action template

--- action-heure.py
+++ action-heure.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-from hermes_python.hermes import Hermes
-from datetime import datetime
-from pytz import timezone
 
-MQTT_IP_ADDR = "localhost"
-MQTT_PORT = 1883
-MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+import subprocess
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
 
 
 def verbalise_hour(i):
@@ -50,11 +67,16 @@ def verbalise_minute(i):
     else:
         return "{0}".format(str(i))
 
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
 
-def intent_received(hermes, intent_message):
-    if intent_message.intent.intent_name == 'Joseph:askTime':
+
+def action_wrapper(hermes, intentMessage, conf):
+    if intentMessage.intent.intent_name == 'Joseph:askTime':
+
         sentence = 'Il est '
-        print(intent_message.intent.intent_name)
+        print(intentMessage.intent.intent_name)
 
         now = datetime.now(timezone('Europe/Paris'))
 
@@ -70,8 +92,10 @@ def intent_received(hermes, intent_message):
         print(sentence)
 
         # hermes.publish_continue_session(intent_message.session_id, sentence, ["Joseph:greetings"])
-        hermes.publish_end_session(intent_message.session_id, sentence)
+        hermes.publish_end_session(intentMessage.session_id, sentence)
 
 
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intents(subscribe_intent_callback).start()
